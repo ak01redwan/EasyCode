d89b16ad244b0b82796d8e8996df02122edd7b81
MohammedSadeqAL-Hriri <mohammed2021@gmail.com>
2023-03-19 17:01:47 +0300

    addAbout

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,5 +1,60 @@
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container marketing mt-5">
+			
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7 m-auto">
+        <h2 class="featurette-heading fw-normal lh-1"> We share <span class="text-muted">knowledge </span></h2>
+        <h2 class="featurette-heading fw-normal lh-1">with the world.</h2>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <img src="../assets/images/img-about.jpg" alt=""  class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  width="500" height="500" >
+      </div>
+    </div>
+  
+    <hr class="featurette-divider">
+    
+    <div class="row m-5">
+      <div class="border-1 m-auto p-2 d-inline col-lg-6">
+        <h3 class="me-3">Our History</h3>
+        <p>When we started EasyCode, our goal was to give anyone in the world the ability to learn the skills they’d need to succeed in the 23st century. We set out to create a new, interactive way of learning, making it engaging, flexible, and accessible for as many people as possible. Since then, we have helped millions of people worldwide unlock modern technical skills and reach their full potential through code.</p>
+      </div>
+      <div class="border-1 m-auto p-2 d-inline col-lg-6">
+        <h3 class="me-3">Our Missoin</h3>
+        <p>We want to create a world where anyone can build something meaningful with technology, and everyone has the learning tools, resources, and opportunities to do so. Code contains a world of possibilities, all that’s required is the curiosity and drive to learn. At EasyCode, we are committed to empowering all people, regardless of where they are in their coding journeys, to continue to learn, grow, and make an impact on the world around them.</p>
+      </div>
+    </div>
+    
+    <h3 class="text-center">Our Team</h3>
+    <hr >
+    <!-- four columns of text below the carousel -->
+    <div class="row text-center justify-content-center ">
+      <div class="col-lg-3 mb-3">
+        <img src="../assets/images/img-user.png" alt="" srcset="" class="bd-placeholder-img rounded-circle bg-info pt-1"  width="150" height="150"  role="img"/>
+        <h4 class="fw-normal">parsonal name</h4>
+        <p>Developer website.</p>					
+      </div>
+      
+      <div class="col-lg-3 mb-3">
+        <img src="../assets/images/img-user.png" alt="" srcset="" class="bd-placeholder-img rounded-circle bg-info pt-1"  width="150" height="150"  role="img"/>
+        <h4 class="fw-normal">parsonal name</h4>
+        <p>Developer website.</p>					
+      </div>
+
+      <div class="col-lg-3 mb-3">
+        <img src="../assets/images/img-user.png" alt="" srcset="" class="bd-placeholder-img rounded-circle bg-info pt-1"  width="150" height="150" role="img"/>
+        <h4 class="fw-normal">parsonal name</h4>
+        <p>Developer website.</p>					
+      </div>
+
+      <div class="col-lg-3 mb-3">
+        <img src="../assets/images/img-user.png" alt="" srcset="" class="bd-placeholder-img rounded-circle bg-info pt-1"  width="150" height="150"  role="img"/>
+        <h4 class="fw-normal">parsonal name</h4>
+        <p>Developer website.</p>					
+      </div>
+    </div>
+
   </div>
 </template>
