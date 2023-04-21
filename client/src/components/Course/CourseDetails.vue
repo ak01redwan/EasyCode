9fc17ab9e097094ff5a1ff6d5ae682bac659f4c7
MohammedSadeqAL-Hriri <mohammed2021@gmail.com>
2023-04-22 00:45:28 +0300

    add courseDetials to components

--- client/src/components/Course/CourseDetails.vue
+++ client/src/components/Course/CourseDetails.vue
@@ -1,3 +1,59 @@
 <template>
-    <h1>this is the course Details</h1>
+    <div class="row" id="upPageCourseDetails">
+        <div class="col-md-6">
+            <img src="https://via.placeholder.com/500x300" alt="Course Image" class="img-fluid">
+            <div class="mt-3">
+                <button type="button" class="btn btn-primary mt-3 ">Enroll Now</button>
+            </div>           
+            <div class="mt-3">
+                <h4>Vote or Evaluation</h4>
+                <p>4.5 stars out of 5</p>
+            </div>
+            <div class="mt-3">
+               <h4>Stages</h4>
+               <ul>
+                    <li>Stage 1</li>
+                    <li>Stage 2</li>
+                    <li>Stage 3</li>
+               </ul>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h1>Course Title</h1>
+            <div class="mt-3">
+                <h4>Description</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis felis vel erat malesuada, vel pellentesque purus semper. Sed nec quam eget mi malesuada semper. Vivamus id elit non elit lacinia hendrerit.</p>
+            </div>
+            <div class="mt-3">
+              <h4>Comments</h4>
+                <ul>
+                    <li>
+                    <div class="d-flex">
+                        <div class="flex-shrink-0">
+                        <img src="https://via.placeholder.com/50x50" alt="User Image" class="rounded-circle" style="width: 50px; height: 50px;">
+                        </div>
+                        <div class="flex-grow-1 ms-3">
+                        <h6>John Doe</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis felis vel erat malesuada, vel pellentesque purus semper. Sed nec quam eget mi malesuada semper.</p>
+                        </div>
+                    </div>
+                    </li>
+                    <li>
+                    <div class="d-flex">
+                        <div class="flex-shrink-0">
+                        <img src="https://via.placeholder.com/50x50" alt="User Image" class="rounded-circle" style="width: 50px; height: 50px;">
+                        </div>
+                        <div class="flex-grow-1 ms-3">
+                        <h6>Jane Smith</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis felis vel erat malesuada, vel pellentesque purus semper. Sed nec quam eget mi malesuada semper.</p>
+                        </div>
+                    </div>
+                    </li>
+                </ul>
+                <div class="fixed-bottom d-flex justify-content-end mb-3 me-3">
+                   <a href="#" class="btn btn-secondary" role="button" data-bs-target="#upPageCourseDetails" data-bs-offset="0" data-bs-toggle="scroll">Page Up</a>
+                </div>
+            </div>
+        </div>
+        </div>
 </template>
