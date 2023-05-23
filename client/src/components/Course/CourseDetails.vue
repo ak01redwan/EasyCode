5b2f05bae3675b6260522d55d7037267ffe56d5a
Abdurhman Khalid Abdullh Redwan <69768077+ak01redwan@users.noreply.github.com>
2023-05-23 20:04:18 +0300

    course(details,students

--- client/src/components/Course/CourseDetails.vue
+++ client/src/components/Course/CourseDetails.vue
@@ -1,26 +1,36 @@
 <template>
     <div class="main-body m-1">
         <div class="row" >           
-            <img src="@/assets/images/man.jpg" alt="" srcset="" class="col-lg-6 col-md-10 m-1">
-           
-            <div class="col-lg-6 col-md-12 m-1">
-                <ul class="list-group text-right">
-                    <li class="list-group-item bg-dark text-white"><strong>Course Details </strong></li>
-                    <li class="list-group-item"><strong>Course Name: </strong>{{ course.name }}</li>
-                    <li class="list-group-item"><strong> Description: </strong>{{ course.Description }}</li>
-                    <li class="list-group-item"><strong>Condetion:</strong>
-                        <ul v-for="courseCondetion in course.Condetion">
-                            <li >{{ courseCondetion }}</li>
-                        </ul>
+          <div class="col-lg-5 col-md-10 mt-4">
+            <img src="@/assets/images/man.jpg" class="w-100 " alt="" srcset="">
+          </div>
+          <div class="col-lg-6 col-md-12 m-1">
+              <ul class="list-group text-right">
+                  <li class="list-group-item bg-dark text-white"><strong>Course Details </strong></li>
+                  <li class="list-group-item"><strong>Course Name: </strong>{{ course.name }}</li>
+                  <li class="list-group-item"><strong> Description: </strong>{{ course.Description }}</li>
+                  <li class="list-group-item"><strong>Condetion:</strong> {{ course.createDate }}</li>
+                  <li class="list-group-item">
+                    <div class="accordion" id="stage-accordion">
+                      <div class="accordion-item">
+                        <h2 class="accordion-header" id="stage1-heading">
+                          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#stage1-collapse" aria-expanded="false" aria-controls="stage1-collapse">
+                            <strong>Stages:</strong>
+                          </button>
+                        </h2>
+                        <div id="stage1-collapse" class="accordion-collapse collapse list-group" aria-labelledby="stage1-heading" data-bs-parent="#stage-accordion">
+                          <div v-for="coursestage in course.courseStage" class="accordion-body list-group-item substage">
+                             {{ coursestage }}
+                          </div>
+                         
+                        </div>
+                      </div>
+                      </div>
                     </li>
-                    <li class="list-group-item"><strong>Stages:</strong>
-                        <ul v-for="courseValue in course.courseValues">
-                            <li >{{ courseValue }}</li>
-                        </ul>
-                    </li>
-                    
-                </ul>
-            </div>
+                  
+              </ul>
+          </div>
+          
         </div>
     </div>
 </template>
@@ -30,7 +40,7 @@ import { Options, Vue } from 'vue-class-component';
   @Options({   
     data () {
       return {
-        course: { id:1, name:"java", imgPath:"@/assets/images/img-user.png",createDate:Date.now,Condetion:["",""],courseValues:["","",""], Description:"advace language"}
+        course: { id:1, name:"java", imgPath:"@/assets/images/img-user.png",createDate:Date.now,courseStage:["Substage 1","Substage 2","Substage 3"], Description:"advace language"}
       }
     }
   })
@@ -38,3 +48,9 @@ import { Options, Vue } from 'vue-class-component';
     [x: string]: any;
   }
 </script>
+<style>
+.substage:hover {
+  background-color: #f5f5f5;
+  cursor: pointer;
+}
+</style>
--- client/src/views/CourseView.vue
+++ client/src/views/CourseView.vue
@@ -18,8 +18,33 @@
         </div>
         <!--Content-->
         <div class="col py-3" style="overflow-y: auto; max-height: 720px;">
+          <!--Getting Course Details-->
           <CourseDetails v-if="currentOption === listOptions[0]" />
+          <!--Getting Course ChatRoom-->
           <ChatRoom v-if="currentOption === listOptions[4]" />
+          <!--Getting Course Stages-->
+          <CourseStage v-if="currentOption === listOptions[1]" v-for="stage in CourseStages"
+            :stageId="stage.stageId" 
+            :stageTitle="stage.stageTitle"
+            :isOpen="stage.isOpen"
+          /> 
+          <!--Getting Students-->
+          <div v-if="currentOption === listOptions[2]" class="row m-0" style="            
+          overflow-y: scroll;
+          ">
+            <div v-for="(student,index) in students" :key="index" class="user-card col-12 p-3">
+              <img src="https://via.placeholder.com/50"  alt="User Avatar" class="bd-placeholder-img rounded-circle">
+              <span class="fw-bold m-4">{{ student.fullname }}</span>
+              <span class="text-muted m-3">{{student.username}}</span>
+              <div class="btn-group float-end">
+                <button class="btn btn-outline-secondary" title="Send Email"><i class="fas fa-envelope"></i></button>
+                <button class="btn btn-outline-secondary" title="View Details"><i class="fas fa-info-circle"></i></button>
+                <button class="btn btn-outline-secondary" title="Edit User"><i class="fas fa-edit"></i></button>
+                <button class="btn btn-outline-danger" title="Delete User"><i class="fas fa-trash"></i></button>
+              </div>
+            </div>
+          </div>
+          
         </div>
       </div>
     </div>
@@ -40,7 +65,7 @@
     data () {
       return {
         currentOption: "CourseDetails",
-        listOptions: ['CourseDetails','MyCourses','MyProjects','CompleteCourses','Settings'],
+        listOptions: ['CourseDetails','MyCourses','MyProjects','CompleteCourses','Settings','Stages'],
         SideBarData: [
             {itemName: 'Details', itemIconClass: 'fa-circle-info'},
             {itemName: 'Stages', itemIconClass: 'fa-circle-play'},
@@ -57,6 +82,21 @@
           {stageId: 5, stageTitle: 'Stage Title 5',isOpen: false},
           {stageId: 6, stageTitle: 'Stage Title 6',isOpen: false},
           {stageId: 7, stageTitle: 'Stage Title 7',isOpen: false},
+        ],
+        students: [
+          {fullname: 'Abdu Khalid Abdullh', username:'AK01REDWAN',type: 'Admin'},
+          {fullname: 'Salem Nagy Khasem', username:'5488', type: 'Student'},
+          {fullname: 'Ahmed Mohammed', username:'AK01REDWAN', type: 'Student'},
+          {fullname: 'Weas Humza', username:'AK01REDWAN', type: 'Student'},
+          {fullname: 'Abdurhman Khald', username:'AK01REDWAN', type: 'Student'},
+          {fullname: 'New Person Name', username:'AK01REDWAN', type: 'Student'},
+          {fullname: 'Another Person Name', username:'AK01REDWAN', type: 'Student'},
+          {fullname: 'Another Person Name', username:'AK01REDWAN', type: 'Admin Supervisor'},
+          {fullname: 'Another Person Name', username:'AK01REDWAN', type: 'Admin Supervisor'},
+          {fullname: 'Another Person Name', username:'AK01REDWAN', type: 'Admin Supervisor'},
+          {fullname: 'Another Person Name', username:'AK01REDWAN', type: 'Normal Supervisor'},
+          {fullname: 'Another Person Name', username:'AK01REDWAN', type: 'Normal Supervisor'},
+          {fullname: 'Another Person Name', username:'AK01REDWAN', type: 'Normal Supervisor'}
         ]
       }
     },
